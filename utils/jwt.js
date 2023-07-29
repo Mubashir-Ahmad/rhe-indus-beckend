84b577ff0ea9be3d15339d02434feac87bc87083
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-07-30 02:46:06 +0500

    30 commit

--- utils/jwt.js
+++ utils/jwt.js
@@ -3,13 +3,14 @@
 const sendtoken = (user,statusCode,res)=>{
     const token = user.getJWTtoken();
     // option cookie
+    console.log('tokeen',token)
     const options={
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE *24*60*60*1000
         ),
-        HttpOnly:true
+        // HttpOnly:true
     };
-    console.log('tokeen',token)
+    
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
         user,token
