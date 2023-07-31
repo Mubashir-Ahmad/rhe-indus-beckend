ef8b67505f29ba28ca091c06c16e7d701c4f1e5d
Mubbashir Ahmad <mubbashirahmad44@gmail.com>
2023-08-01 02:48:55 +0500

    54 commit

--- app.js
+++ app.js
@@ -19,16 +19,13 @@ dotenv.config({path:"beckend/config/config.env"});
 
 // Increase the request size limit to 10MB
 app.use(express.json({ limit: '10mb' }));
-const allowedOrigins = ['*']; // Add localhost for development
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 // dotenv.config({path:'beckend/config/config.env'})
 app.use(cookieParser())
 app.use(express.json())
